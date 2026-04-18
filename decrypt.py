dc9c42aa431dce205b5bdd3702c13094083a78bf
casamvout <dmitrijbogila@gmail.com>
2026-04-18 14:11:21 +0300

    Some global fixes Fixed problem where decrypt everytime spoke data is corrupted

--- decrypt.py
+++ decrypt.py
@@ -15,29 +15,30 @@ def decrypt_folder(password, folder):
     with open(fr"{folder}\pass.hash", "r") as f:
         read_password = cryptolibo.decrypt.aes_gcm(key_password, f.read())
     with open(fr"{folder}\pass.salt", "r")as f:
-        read_salt = cryptolibo.decrypt.aes_gcm(key_password, bytes.fromhex(f.read()))
-    hashed_password, _ = cryptolibo.hash.pbkdf2(password, read_salt, length=64)
+        read_salt = cryptolibo.decrypt.aes_gcm(key_password, (f.read()))
+    hashed_password, _ = cryptolibo.hash.pbkdf2(password, bytes.fromhex(read_salt), length=64)
     if hashed_password != read_password:
-        return "Decryption Error: Passwords do not match"
+        raise "Decryption Error: Passwords do not match"
     else:
         # Protection against interruption of code execution during decryption
         pathlib.Path(fr"{folder}\tmp").mkdir(exist_ok=True)
         for file in folder.rglob("*"):
+            if file.name in ("pass.hash", "pass.salt"):
+                continue
             if not file.is_file():
                 continue
             if file.name in os.listdir(fr"{folder}\tmp"):
                 continue
             with open(fr"{folder}\tmp\{file.name}", "w"):
                 pass
-            if file.name in ("pass.hash", "pass.salt"):
-                continue
-            with open(file, "rb") as f:
+            with open(file, "r") as f:
                 data = f.read()
-                decrypted_bytes = cryptolibo.decrypt.chacha20_poly1305(key_password, data)
+                decrypted_data = cryptolibo.decrypt.chacha20_poly1305(key_password, data)
             with open(file, "wb") as f:
                 # encode needed! chacha20_poly1305 always returns base64 (str)!
-                f.write(decrypted_bytes.encode('utf-8'))
+                f.write(decrypted_data.encode('utf-8'))
             print(f"\rDecrypted {curr_count}/{total} Files", end="", flush=True)
         misc_utils.safe_delete(fr"{folder}\pass.hash")
         misc_utils.safe_delete(fr"{folder}\pass.salt")
         shutil.rmtree(fr"{folder}\tmp")
+        print("\n")
--- encrypt.py
+++ encrypt.py
@@ -12,7 +12,13 @@ def encrypt_folder(password, folder):
     iteration = round(password_len)
     # we don't need salt, we just need to stretch the password
     key_password, _ = cryptolibo.hash.pbkdf2(password, salt=password, length=128, iterations=iteration)
+    with open (fr"{folder}\pass.hash", "w") as f:
+        f.write(cryptolibo.encrypt.aes_gcm(key_password, hash_password))
+    with open (fr"{folder}\pass.salt", "w") as f:
+        f.write(cryptolibo.encrypt.aes_gcm(key_password, salt))
     for file in folder.rglob("*"):
+        if file.name in ("pass.hash", "pass.salt"):
+            continue
         with open(file, "rb") as f:
             read_bytes = f.read()
             encrypted_bytes = cryptolibo.encrypt.chacha20_poly1305(key_password, read_bytes)
@@ -21,11 +27,7 @@ def encrypt_folder(password, folder):
         with open(file, "wb") as f:
             # encode needed! chacha20_poly1305 always returns base64 (str)!
             f.write(encrypted_bytes.encode('utf-8'))
-        print(f"\rEncrypted {curr_count}/{total - 2} Files", end="", flush=True)
+        print(f"\rEncrypted {curr_count}/{total} Files", end="", flush=True)
         curr_count += 1
         misc_utils.safe_delete(f"{file}.backup")
-    with open (fr"{folder}\pass.hash", "w") as f:
-        f.write(cryptolibo.encrypt.aes_gcm(key_password, hash_password))
-    with open (fr"{folder}\pass.salt", "w") as f:
-        f.write(cryptolibo.encrypt.aes_gcm(key_password, salt))
     print("\n")
